d480686826763bc82000894d03c422b8ea3e94c2
Etelos005 <marcetel64@gmail.com>
2025-07-02 12:39:22 +0300

    nextJs

--- components/DreamCompany.js
+++ components/DreamCompany.js
@@ -3,7 +3,6 @@ export default function DreamCompany() {
     <section className="py-12 md:py-20 px-4 bg-white">
       <div className="container mx-auto px-4 md:px-6 grid grid-cols-1 md:grid-cols-12 items-center gap-8 min-h-[450px]">
         
-        {/* Image Section */}
         <div className="md:col-span-6 flex justify-center md:justify-start">
           <img
             src="/Images/thumbsup.png"
@@ -12,12 +11,11 @@ export default function DreamCompany() {
           />
         </div>
 
-        {/* Text Section */}
-        <div className="md:col-span-6 flex flex-col justify-center gap-4 text-center md:text-left items-center md:items-start md:-ml-[120px]">
-          <h2 className="text-2xl md:text-5xl font-bold text-black">
+        <div className="md:col-span-6 flex flex-col gap-4 text-center md:text-left items-center md:items-start md:-ml-[120px] w-full">
+          <h2 className="text-2xl md:text-5xl font-bold text-black w-full">
             We will help you to become an employee in your dream company
           </h2>
-          <p className="text-gray-600 max-w-md">
+          <p className="text-gray-600 w-full">
             Everyone has a high goal in mind: "I want to work at X company, it would be my dream!". Well no need to worry folks, because JobNow has got you covered with every job that you can possibly think of so don't hesitate to search for your dream job
           </p>
           <button className="mt-2 border border-[#00D5AE] text-[#00D5AE] hover:bg-[#00D5AE] hover:text-white transition px-5 py-2 rounded-md text-sm font-medium">
