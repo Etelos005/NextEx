8bdfecd477c48046bc5013339f40e88de3163749
Etelos005 <marcetel64@gmail.com>
2025-07-02 10:15:12 +0300

    nextJs

--- components/DreamCompany.js
+++ components/DreamCompany.js
@@ -1,8 +1,10 @@
 export default function DreamCompany() {
   return (
     <section className="py-20 px-6 bg-white">
-      <div className="container mx-auto px-6 grid grid-cols-3 md:grid-cols-12 items-center gap-0">
-        <div className="md:col-span-6 flex justify-center md:justify-start ">
+      <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-12 items-center gap-12">
+
+        {/* Image always on top for mobile */}
+        <div className="md:col-span-6 flex justify-center">
           <img
             src="/Images/thumbsup.png"
             alt="Thumbs up team"
@@ -10,17 +12,19 @@ export default function DreamCompany() {
           />
         </div>
 
-        <div className="md:col-span-6 -ml-30 flex flex-col gap-4 text-center md:text-left items-center md:items-start">
+        {/* Text block */}
+        <div className="md:col-span-6 flex flex-col gap-4 items-center text-center md:text-left md:items-start">
           <h2 className="text-2xl md:text-3xl font-bold text-black">
             We will help you to become an employee in your dream company
           </h2>
-          <p className="text-gray-600">
-            Everyone has a high goal in mind: "I want to work at X company, it would be my dream!". Well no need to worry folks, because JobNow has got you covered with every job that you can possibly think of so don't hesitate to search for your dream job
+          <p className="text-gray-500 max-w-md text-sm">
+            Everyone has a high goal in mind: "I want to work at X company, it would be my dream!". Well no need to worry folks, because JobNow has got you covered with every job that you can possibly think of so don't hesitate to search for your dream job.
           </p>
-          <button className="mt-2 border border-[#00D5AE] text-[#00D5AE] hover:bg-[#00D5AE] hover:text-white transition px-5 py-2 rounded-md text-sm font-medium mx-auto md:mx-0 cursor-pointer">
+          <button className="mt-2 border border-[#00D5AE] text-[#00D5AE] hover:bg-[#00D5AE] hover:text-white transition px-6 py-2 rounded-md text-sm font-medium">
             Learn More
           </button>
         </div>
+
       </div>
     </section>
   )
